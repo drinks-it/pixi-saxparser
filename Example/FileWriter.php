8cda465d2a70332f645c2746b0886e2a2e80c9ae
fseidl <devnull@localhost>
2013-08-20 11:50:38 +0200

    Added comments to files for easier understanding.

--- Example/FileWriter.php
+++ Example/FileWriter.php
@@ -2,19 +2,54 @@
 
 namespace Example\Subscriber;
 
+/**
+ * Example FileWriter, which can be used as subscriber to the
+ * xml parser class.
+ */
 class FileWriter implements \Symfony\Component\EventDispatcher\EventSubscriberInterface
 {
     protected $fp;
     protected $filename;
     protected $content;
-    
+
+    /**
+     * Mandatory method, which tells the dispatcher, which events
+     * this subsriber is listening to and what method in the
+     * subscriber will be called. Additionally defines the priority
+     * of the subscriber methods.
+     *
+     * @access public
+     * @param void
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            "tag.open" => array("onTagOpen", 0),
+            "tag.data" => array("onTagData", 0),
+            "tag.close" => array("onTagClose", 0)
+        );
+    }
+
+    /**
+     * Sets the filename property
+     */
     public function __construct($filename)
     {
         $this->filename = $filename;
     }
 
+    /**
+     * This method adds some additional information
+     * to the data and creates a string, which will be
+     * written to the file.
+     *
+     * @access public
+     * @param array $event
+     * @return void
+     */
     public function onTagOpen($event)
-    {       
+    {
         switch($event['tagName']) {
             case 'BREAKFAST_MENU':
                 $this->fp = fopen($this->filename, 'w');
@@ -31,12 +66,29 @@ class FileWriter implements \Symfony\Component\EventDispatcher\EventSubscriberIn
                 break;
         }
     }
-    
+
+    /**
+     * Is executed when the data of an xml tag is parsed.
+     * It just trims the data, without further modification.
+     *
+     * @access public
+     * @param array $event
+     * @return void
+     */
     public function onTagData($event)
     {
         $this->content .= trim($event['data']);
     }
-    
+
+    /**
+     * This method adds some additional information
+     * to the data and creates a string, which will be
+     * written to the file.
+     *
+     * @access public
+     * @param array $event
+     * @return void
+     */
     public function onTagClose($event)
     {
         switch ($event['tagName']) {
@@ -59,16 +111,5 @@ class FileWriter implements \Symfony\Component\EventDispatcher\EventSubscriberIn
                 $this->content .= '';
                 break;
         }
-    }        
-    
-    public static function getSubscribedEvents()
-    {
-        return array(
-            "tag.open" => array("onTagOpen", 0),
-            "tag.data" => array("onTagData", 0),
-            "tag.close" => array("onTagClose", 0)
-        );
     }
-    
-    
 }
--- Example/index.php
+++ Example/index.php
@@ -1,20 +1,22 @@
 <?php
 
+// Including the autoloader and an Subscriber
 require __DIR__.'/../vendor/autoload.php';
 require "FileWriter.php";
 
 $exampleXml = fopen("food.xml", "r");
 
+// Inizializing the parser and adding the included subscriber
 $saxParser = new pixi\Xml\Parser\Sax();
 $streamParser->dispatcher->addSubscriber(new Example\Subscriber\FileWriter("FoodInfo.txt"));
 
-
+// Parsing the xml by lines
 while(!feof($exampleXml)) {
     $streamParser->parse(fread($exampleXml, 4096));
 }
 fclose($exampleXml);
 
-
+// Reading the results of the FileWriter subscriber
 $newFp = fopen("FoodInfo.txt", "r");
 while(!feof($newFp)) {
     echo fread($newFp, 4096);
